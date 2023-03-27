9295c245999f9745047659f3d275eed1b2e48754
esaccar <carlo.saccardi@ericsson.com>
2023-03-27 17:52:42 +0200

    working privGAN

--- gan_models/dcgan/privDCGAN.py
+++ gan_models/dcgan/privDCGAN.py
@@ -33,25 +33,18 @@ parser.add_argument('--dp_delay', type=int, default=100, help='starting epoch fo
 parser.add_argument('--out_size', type=int, help='number of output images')
 parser.add_argument('--beta1', type=float, default=0.5 , help='beta1 for adam. default=0.5')
 parser.add_argument('--beta2', type=float, default=0.999, help='beta2 for adam. default=0.999')
-parser.add_argument('--privacy_ratio', type=float, default=0.5, help='privacy ratio')
 parser.add_argument('--data_name', type=str, default='miniCelebA', help='name of the dataset, either miniCelebA or CelebA')
 parser.add_argument("--wandb", default=None, help="Specify project name to log using WandB")
 parser.add_argument('--local_config', default=None, help='path to config file')
 
-parser.add_argument("--PATH", type=str, default=os.path.join(os.getcwd(),'model_save', 'dcgan'), help="Directory to save model")
-parser.add_argument("--PATH_syn_data", type=str, default=os.path.join(os.getcwd(), 'syn_data', 'dcgan'), help="Directory to save synthetic data")
+parser.add_argument("--PATH", type=str, default=os.path.join(os.getcwd(),'model_save', 'privDCGAN'), help="Directory to save model")
+parser.add_argument("--PATH_syn_data", type=str, default=os.path.join(os.getcwd(), 'syn_data', 'privDCGAN'), help="Directory to save synthetic data")
 
 parser.add_argument("--save_model", type=bool, default=True, help="Save model or not")
 parser.add_argument("--saved_model_name", type=str, default=None, help="Saved model name")
-
-parser.add_argument("--training", type=bool, default=True, help="Training status")
-parser.add_argument("--resume", type=bool, default=False, help="Training status")
-parser.add_argument("--finetuning", type=bool, default=False, help="Training status")
-parser.add_argument("--generate", type=bool, default=True, help="Generating Sythetic Data")
-parser.add_argument("--evaluate", type=bool, default=False, help="Evaluation status")
-
 #### PRIVGAN PARAMETERS ####
 parser.add_argument("--N_splits", type=bool, default=2, help="number of gen-disc pairs")
+parser.add_argument('--privacy_ratio', type=float, default=0.5, help='privacy ratio')
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -96,9 +89,6 @@ torch.autograd.set_detect_anomaly(True)
 def main():
     #TODO: add seed for reproducibility and initialization of weights
     print(args)
-    
-    now = datetime.datetime.now() # To create a unique folder for each run
-    timestamp = now.strftime("_%Y_%m_%d__%H_%M_%S")  # To create a unique folder for each run
     
     gen = Generator(args.nz, args.nc, args.ngf).to(device)
     disc = Discriminator(args.nc, args.ndf).to(device)
@@ -146,6 +136,10 @@ def main():
 
 
 def train_privGAN(genS, discS, private_disc, opt_gen, opt_disc, opt_private_disc, t, criterion, loss_fn):
+
+    now = datetime.datetime.now() # To create a unique folder for each run
+    timestamp = now.strftime("_%Y_%m_%d__%H_%M_%S")  # To create a unique folder for each run
+
     #train generator and discriminator for gen_epochs
     batchCount = int(t // args.batch_size) + 1
 
@@ -154,20 +148,20 @@ def train_privGAN(genS, discS, private_disc, opt_gen, opt_disc, opt_private_disc
         d_t = np.zeros((args.N_splits, batchCount))
         dp_t = np.zeros(batchCount * args.N_splits)
         g_t = np.zeros(batchCount * args.N_splits)
-        privD_dataset = []
-        gen_dataset = []
 
         #for each dataloader in dataloader_N, train the discriminator
         for split in range(args.N_splits):
 
-            imgsF_split = []
+            l = list(range(args.N_splits))
+            del(l[split])
+            gen_y =  np.random.choice( l, ( len( X[split] ) , ) )
+            gen_y = torch.tensor(gen_y, dtype=torch.float32).to(device)
 
             for i, (imgs, _) in enumerate(X_loaders[split]):
 
                 imgs = imgs.to(device)
                 noise = torch.randn(imgs.shape[0], args.nz, 1, 1).to(device)
                 fake = genS[split](noise)
-                imgsF_split += [fake]#no detach here as we want to train the generator
 
                 # train discriminator --> max log(D(x)) + log(1 - D(G(z)))
                 disc_real = discS[split](imgs).reshape(-1)
@@ -176,78 +170,92 @@ def train_privGAN(genS, discS, private_disc, opt_gen, opt_disc, opt_private_disc
                 lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
                 lossD = (lossD_real + lossD_fake) / 2
                 discS[split].zero_grad()
-                lossD.backward(retain_graph=True)
+                lossD.backward()
                 opt_disc.step()
-
                 d_t[split, i] = lossD.item()
 
-            #prepare labels for generator: random numbers as the goal is not only too fool 
-            #the discriminator but also the private discriminator. A random lable makes the
-            #generator generate images that are not too similar to the ones from the same split
-            X_fakes_split = torch.cat(imgsF_split, axis=0)
-            l = list(range(args.N_splits))
-            del(l[split])
-            gen_y =  np.random.choice( l, ( len( X_fakes_split ) , ) ) 
-            gen_y = torch.tensor(gen_y, dtype=torch.float32).to(device)
-            gen_dataset.append(TensorDataset(X_fakes_split, gen_y))
 
-            if epoch > args.dp_delay:
-                privD_y = torch.tensor([split]*len(X[split]), dtype=torch.float32).to(device)
-                privD_dataset.append(TensorDataset(X_fakes_split, privD_y))
+                #train private discriminator. Label is the split index of the data. Goal is to determine which
+                #generator produced the data. Hence, we use the split index as the label
+                if epoch > args.dp_delay:
+                    output = private_disc(fake.detach()).reshape(-1)
+                    labels_Dp = torch.tensor([split]*imgs.shape[0], dtype=torch.float32).to(device)
 
-        if epoch > args.dp_delay:
+                    loss_Dp = loss_fn(output, labels_Dp)
 
-            #concatenate all the data from all the splits
-            imgsF_dataset = ConcatDataset(privD_dataset)
-            imgsF_dataloader = torch.utils.data.DataLoader(imgsF_dataset, batch_size=args.batch_size, shuffle=True)
+                    private_disc.zero_grad()
+                    loss_Dp.backward()
 
-            #train private discriminator with yDisc2 as labels
-            for i, (imgs, lables) in enumerate(imgsF_dataloader):
+                    opt_private_disc.step()
 
-                imgs = imgs.to(device)
-                lables = lables.to(device)
-                output = private_disc(imgs).reshape(-1)
-                lossD_private = loss_fn(output, lables)
+                    dp_t[i] = loss_Dp.item()
 
-                private_disc.zero_grad()
-                lossD_private.backward(retain_graph=True)
-                
-                opt_private_disc.step()
 
-                dp_t[i] = lossD_private.item()
-
-        #train generators: goal is to fool the discriminator and the private discriminator
-        for split in range(args.N_splits):
-
-            gen_dataset_train = ConcatDataset(gen_dataset)
-            gen_dataset_train = torch.utils.data.DataLoader(gen_dataset_train, batch_size=args.batch_size, shuffle=True)
-
-            for i, (imgs, lables) in enumerate(gen_dataset_train):
-
-                imgs = imgs.to(device)#this is the fake image, make sure gradient flows back to generator.
-                output1 = discS[split](imgs).reshape(-1)
-                output2 = private_disc(imgs).reshape(-1)
+                # train generator. Goal is to fool the discriminator and the private discriminator.
+                # Hence, we use different split index as the label for loss_fn, and ones as the label for
+                #criterion.
+                output1 = discS[split](fake).reshape(-1)
+                output2 = private_disc(fake).reshape(-1)
+                labels_gen = gen_y[i*imgs.shape[0]:(i+1)*imgs.shape[0]] #labels is a different split index to fool the private discriminator
 
                 lossG_1 = criterion(output1, torch.ones_like(output1))
-                lossG_2 = args.privacy_ratio * loss_fn(output2, lables)
+                lossG_2 = args.privacy_ratio * loss_fn(output2, labels_gen.to(device))
 
                 lossG = lossG_1 + lossG_2
 
                 genS[split].zero_grad()
-                lossG.backward()#TODO: bug here
+                lossG.backward()
                 opt_gen.step()
-            
-                g_t[i] = lossG.item()
+
+                g_t[i] = lossG.item()    
+
+            #prepare labels for generator: random numbers as the goal is not only too fool 
+            #the discriminator but also the private discriminator. A random lable makes the
+            #generator generate images that are not too similar to the ones from the same split
+
 
         with torch.no_grad():
             print(f"Epoch {epoch} of {args.num_epochs} complete. Loss D: {d_t.mean()}, Loss DP: {dp_t.mean()}, Loss G: {g_t.mean()}")
+            if args.wandb:
+                wandb.log({"D_loss": d_t.mean(), "DP_loss": dp_t.mean(), "G_loss": g_t.mean()})
+
+            if epoch % 10 == 0:
+                fixed_noise = torch.randn(1, args.nz, 1, 1, device=device)
+                fake_wb = genS[0](fixed_noise).detach().cpu()#shape (1, 3, 64, 64)
+                grid = torchvision.utils.make_grid(fake_wb[0], normalize=True)
+                wandb.log({"generated_images": wandb.Image(grid, caption="epoch: {}".format(epoch))})
+
+
+    #save the models
+    if args.save_models:
+        dirname = os.path.join(args.PATH, timestamp)
+        for i in range(args.N_splits):
+            torch.save(genS[i].state_dict(), os.path.join(dirname, f"gen{i}.pt") )
+            torch.save(discS[i].state_dict(), os.path.join(dirname, f"disc{i}.pt") )
+        torch.save(private_disc.state_dict(), os.path.join(dirname, "private_disc.pt"))
+
+
         
 
+def update_args(args, config_dict):
+    for key, val in config_dict.items():
+        setattr(args, key, val)  
 
-                
 
+if __name__ == '__main__':
 
+    if args.local_config is not None:
+        with open(str(args.local_config), "r") as f:
+            config = yaml.safe_load(f)
+        update_args(args, config)
+        if args.wandb:
+            wandb_config = vars(args)
+            run = wandb.init(project=str(args.wandb), entity="thesis_carlo", config=wandb_config)
+            # update_args(args, dict(run.config))
+    else:
+        warnings.warn("No config file was provided. Using default parameters.")
 
+    main()
 
 if __name__ == '__main__':
     main()
