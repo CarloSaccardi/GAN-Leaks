320847766877a357183eb3c35a401673f5aee6fa
CarloSaccardi <carlosaccardi.99@gmail.com>
2023-04-16 11:59:42 +0000

    privDCGAN first big run

--- gan_models/dcgan/privDCGAN.py
+++ gan_models/dcgan/privDCGAN.py
@@ -15,6 +15,7 @@ import yaml
 import warnings
 import datetime
 import torchvision
+from utils import CustomDataset
 
 from model_torch import Generator, Discriminator, PrivateDiscriminator, initialize_weights
 
@@ -42,6 +43,9 @@ parser.add_argument("--PATH_syn_data", type=str, default=os.path.join(os.getcwd(
 
 parser.add_argument("--save_model", type=bool, default=True, help="Save model or not")
 parser.add_argument("--saved_model_name", type=str, default=None, help="Saved model name")
+
+parser.add_argument("--training", type=bool, default=True, help="Training status")
+parser.add_argument("--generate", type=bool, default=True, help="Generating Sythetic Data")
 #### PRIVGAN PARAMETERS ####
 parser.add_argument("--N_splits", type=bool, default=2, help="number of gen-disc pairs")
 parser.add_argument('--privacy_ratio', type=float, default=0.5, help='privacy ratio')
@@ -50,38 +54,6 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 args = parser.parse_args()
 
-
-transform = transforms.Compose([
-                transforms.Resize((args.image_size, args.image_size)),
-                transforms.ToTensor(),
-                transforms.Normalize([0.5 for _ in range(args.nc)], [0.5 for _ in range(args.nc)])
-                ])
-
-
-dataset = dset.ImageFolder(root= os.path.join('data', args.data_name), transform=transform)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
-
-
-####################### SPLIT DATASET ############################
-X = []
-t = len(dataset)//args.N_splits
-y_train = []
-X_loaders = []
-
-for i in range(args.N_splits):
-    if i<args.N_splits-1:
-        dataset_split = Subset(dataset, range(i*t, (i+1)*t))
-        X_loaders += [torch.utils.data.DataLoader(dataset_split, batch_size=args.batch_size, shuffle=False)]
-        X += [dataset_split]
-        y_train += [i]*t
-    else:
-        dataset_split = Subset(dataset, range(i*t, len(dataset)))
-        X += [dataset_split]
-        X_loaders += [torch.utils.data.DataLoader(dataset_split, batch_size=args.batch_size, shuffle=False)]
-        y_train += [i]*(len(dataset)-i*t)
-
-y_train = np.array(y_train) + 0.0 
-
 fixed_noise = torch.randn(1, args.nz, 1, 1, device=device)
 
 torch.autograd.set_detect_anomaly(True)
@@ -89,6 +61,35 @@ torch.autograd.set_detect_anomaly(True)
 def main():
     #TODO: add seed for reproducibility and initialization of weights
     print(args)
+
+    transform = transforms.Compose([
+                    transforms.Resize((args.image_size, args.image_size)),
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.5 for _ in range(args.nc)], [0.5 for _ in range(args.nc)])
+                    ])
+    dataset = CustomDataset(root= os.path.join('data', args.data_name), transform=transform, n = 10000)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+
+
+    ####################### SPLIT DATASET ############################
+    X = []
+    t = len(dataset)//args.N_splits
+    y_train = []
+    X_loaders = []
+
+    for i in range(args.N_splits):
+        if i<args.N_splits-1:
+            dataset_split = Subset(dataset, range(i*t, (i+1)*t))
+            X_loaders += [torch.utils.data.DataLoader(dataset_split, batch_size=args.batch_size, shuffle=False)]
+            X += [dataset_split]
+            y_train += [i]*t
+        else:
+            dataset_split = Subset(dataset, range(i*t, len(dataset)))
+            X += [dataset_split]
+            X_loaders += [torch.utils.data.DataLoader(dataset_split, batch_size=args.batch_size, shuffle=False)]
+            y_train += [i]*(len(dataset)-i*t)
+
+    y_train = np.array(y_train) + 0.0 
     
     gen = Generator(args.nz, args.nc, args.ngf).to(device)
     disc = Discriminator(args.nc, args.ndf).to(device)
@@ -103,39 +104,43 @@ def main():
     loss_fn = torch.nn.CrossEntropyLoss()
     criterion = torch.nn.BCELoss()
 
-    gen.train()
-    disc.train()
-    private_disc.train()
+    if args.training:
 
-    #pre-train private discriminator for disc_epochs. Use Y_train as labels
-    for epoch in range(args.disc_epochs):
-        for i, (imgs, _) in enumerate(dataloader):
-            imgs = imgs.to(device)
-            batch_size = imgs.shape[0]
-            lables = y_train[i*batch_size:(i+1)*batch_size]
-            lables = torch.tensor(lables).to(device)
+        gen.train()
+        disc.train()
+        private_disc.train()
 
-            opt_private_disc.zero_grad()
-            output = private_disc(imgs).reshape(-1)
+        #pre-train private discriminator for disc_epochs. Use Y_train as labels
+        for epoch in range(args.disc_epochs):
+            for i, (imgs) in enumerate(dataloader):
+                imgs = imgs.to(device)
+                batch_size = imgs.shape[0]
+                lables = y_train[i*batch_size:(i+1)*batch_size]
+                lables = torch.tensor(lables).to(device)
 
-            loss = loss_fn(output, lables)
-            loss.backward()
+                opt_private_disc.zero_grad()
+                output = private_disc(imgs).reshape(-1)
 
-            opt_private_disc.step()
+                loss = loss_fn(output, lables)
+                loss.backward()
 
-
-    train_privGAN(genS = [gen]*args.N_splits ,
-                   discS = [disc]*args.N_splits, 
-                   criterion = criterion,
-                   opt_gen = opt_gen, 
-                   opt_disc = opt_disc, 
-                   private_disc=private_disc,
-                   opt_private_disc = opt_private_disc,
-                   t = t, 
-                   loss_fn = loss_fn)
+                opt_private_disc.step()
 
 
-def train_privGAN(genS, discS, private_disc, opt_gen, opt_disc, opt_private_disc, t, criterion, loss_fn):
+        train_privGAN(genS = [gen]*args.N_splits ,
+                    discS = [disc]*args.N_splits, 
+                    criterion = criterion,
+                    opt_gen = opt_gen, 
+                    opt_disc = opt_disc, 
+                    private_disc=private_disc,
+                    opt_private_disc = opt_private_disc,
+                    t = t, 
+                    loss_fn = loss_fn,
+                    X_loaders = X_loaders,
+                    X = X)
+
+
+def train_privGAN(genS, discS, private_disc, opt_gen, opt_disc, opt_private_disc, t, criterion, loss_fn, X_loaders, X):
 
     now = datetime.datetime.now() # To create a unique folder for each run
     timestamp = now.strftime("_%Y_%m_%d__%H_%M_%S")  # To create a unique folder for each run
@@ -157,7 +162,7 @@ def train_privGAN(genS, discS, private_disc, opt_gen, opt_disc, opt_private_disc
             gen_y =  np.random.choice( l, ( len( X[split] ) , ) )
             gen_y = torch.tensor(gen_y, dtype=torch.float32).to(device)
 
-            for i, (imgs, _) in enumerate(X_loaders[split]):
+            for i, (imgs) in enumerate(X_loaders[split]):
 
                 imgs = imgs.to(device)
                 noise = torch.randn(imgs.shape[0], args.nz, 1, 1).to(device)
@@ -227,15 +232,51 @@ def train_privGAN(genS, discS, private_disc, opt_gen, opt_disc, opt_private_disc
 
 
     #save the models
-    if args.save_models:
+    if args.save_model:
         dirname = os.path.join(args.PATH, timestamp)
+        os.makedirs(dirname, exist_ok=True)
         for i in range(args.N_splits):
-            torch.save(genS[i].state_dict(), os.path.join(dirname, f"gen{i}.pt") )
-            torch.save(discS[i].state_dict(), os.path.join(dirname, f"disc{i}.pt") )
-        torch.save(private_disc.state_dict(), os.path.join(dirname, "private_disc.pt"))
+            torch.save(genS[i].state_dict(), os.path.join(dirname, f"gen{i}.pth") )
+            torch.save(discS[i].state_dict(), os.path.join(dirname, f"disc{i}.pth") )
+        torch.save(private_disc.state_dict(), os.path.join(dirname, "private_disc.pth"))
 
+    if args.generate:
+        #load the saved model, generate args.batch_size synthetic data, and save them as .npz file
 
+        gen = Generator(args.nz, args.nc, args.ngf).to(device)
+
+        if args.training:
+            gen.load_state_dict(torch.load(os.path.join(dirname, "gen0.pth")))
+        else:
+            assert args.saved_model_name is not None, "Please specify the saved model name" 
+            assert args.wandb == None, "No need to load anything to wand when only generating synthetic data"
+            gen.load_state_dict(torch.load(os.path.join(args.saved_model_name, "generator.pth")))
         
+        gen.eval()
+
+        with torch.no_grad():
+            noise = torch.randn(args.batch_size, args.nz, 1, 1, device=device)
+            normalize = transforms.Normalize(mean=[-1, -1, -1], std=[2, 2, 2])
+            to_pil = transforms.ToPILImage()
+
+            fake = gen(noise).detach().cpu()
+            fake = normalize(fake)
+
+            dirname = os.path.join(args.PATH_syn_data , 'npz_images', timestamp)
+            os.makedirs(dirname, exist_ok=True)
+            np.savez(os.path.join(dirname, "dcgan_synthetic_data.npz"), fake=fake)
+
+            dirname = os.path.join(args.PATH_syn_data , 'npz_noise', timestamp)
+            os.makedirs(dirname, exist_ok=True)
+            np.savez(os.path.join(dirname, "dcgan_noise.npz"), noise=noise.cpu())
+
+            dirname = os.path.join(args.PATH_syn_data , 'png_images', timestamp)
+            os.makedirs(dirname, exist_ok=True)
+            for i, img in enumerate(fake):
+                pil_img = to_pil(img)
+                save_path = os.path.join(dirname, f"image_{i}.png")
+                pil_img.save(save_path)        
+
 
 def update_args(args, config_dict):
     for key, val in config_dict.items():
@@ -255,7 +296,4 @@ if __name__ == '__main__':
     else:
         warnings.warn("No config file was provided. Using default parameters.")
 
-    main()
-
-if __name__ == '__main__':
     main()
